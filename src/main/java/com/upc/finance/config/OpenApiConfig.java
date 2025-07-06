b339082cb833208c1b6974bf46beb009145f1366
Andrea O'Higgins <u20221b178@upc.edu.pe>
2025-07-05 22:57:57 -0500

    Update OpenApiConfig.java

--- src/main/java/com/upc/finance/config/OpenApiConfig.java
+++ src/main/java/com/upc/finance/config/OpenApiConfig.java
@@ -1,11 +1,13 @@
 package com.upc.finance.config;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -16,7 +18,8 @@ public class OpenApiConfig {
                         .title("Finance Platform")
                         .description("Finance Platform application REST API documentation")
                         .version("v1.0.0")
-                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
+                        .servers(List.of(new Server().url("https://accurate-cat-production.up.railway.app"))
 
                 );
 
